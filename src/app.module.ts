15bd345e751f0102197ff48b708152be34b7980d
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-16 09:02:50 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClassesModule } from './classes/classes.module';
+import { StudentsModule } from './students/students.module';
 
 @Module({
-  imports: [],
+  imports: [ClassesModule, StudentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
