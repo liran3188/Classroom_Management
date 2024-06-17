d540bf354779e435c5bcfbec648f065d970aa2d8
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-17 16:52:25 +0300

    Update classes.module.ts

--- src/classes/classes.module.ts
+++ src/classes/classes.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { ClassesController } from './classes.controller';
 import { ClassesService } from './classes.service';
+import { ClassRepository } from './classes.repository';
+import { StudentClass } from 'src/StudentClass.model';
 
 @Module({
+    imports: [StudentClass],
     controllers: [ClassesController],
-    providers: [ClassesService],
+    providers: [ClassesService, ClassRepository],
 })
 export class ClassesModule {}
