1b76f2f933fab3d1de655cbce1513d8432b2f8e6
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-17 16:51:50 +0300

    Update class.model.ts

--- src/classes/class.model.ts
+++ src/classes/class.model.ts
@@ -1,5 +1,6 @@
 import { Column, Model, Table } from 'sequelize-typescript';
 
+@Table
 export class Class extends Model<Class>{
     @Column({ primaryKey: true, autoIncrement: true })
     id: number;
@@ -10,10 +11,12 @@ export class Class extends Model<Class>{
     @Column
     capacity: number;
 
-    constructor(id: number, name: string, capacity: number) {
-        super();
+    constructor(id?: number, name?: string, capacity?: number) {
+      super();
+      if (id !== undefined && name !== undefined && capacity !== undefined) {
         this.id = id;
         this.name = name;
         this.capacity = capacity;
       }
+    }
 }
