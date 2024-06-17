71fe6922bb947f5638a4b9a6164b59b0bc9a48a6
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-17 16:54:19 +0300

    Update students.repository.ts

--- src/students/students.repository.ts
+++ src/students/students.repository.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Student } from './student.model';
+import { StudentClass } from '../StudentClass.model';
+import { db, sequelize } from '../db'
 
 @Injectable()
 export class StudentRepository {
   constructor(
     @InjectModel(Student)
     private studentModel: typeof Student,
+    @InjectModel(StudentClass)
+    private studentClassModel: typeof StudentClass,
   ) {}
 
   async findAll(): Promise<Student[]> {
@@ -23,6 +27,19 @@ export class StudentRepository {
 
   async delete(id: number): Promise<number> {
     const deletedRows = await this.studentModel.destroy({ where: { id } });
+    return deletedRows;
+  }
+
+  async getClasses(id: number): Promise<Number[]> {
+    const query = `SELECT class_id FROM public.student_classes WHERE student_id = ${id}`;
+    return db.query(query, { type: sequelize.QueryTypes.SELECT })
+    .then(results => {
+      return (results);
+    })
+  }
+
+  async deleteConnection(id: number): Promise<number> {
+    const deletedRows = await this.studentClassModel.destroy({ where: { studentId: id } });
     return deletedRows;
   }
 }
