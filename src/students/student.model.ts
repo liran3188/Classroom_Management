ad8facf07eb63cb49c33ea2f32f6d01569c5c039
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-17 08:55:59 +0300

    Update student.model.ts

--- src/students/student.model.ts
+++ src/students/student.model.ts
@@ -1,9 +1,27 @@
-export class Student {
-    constructor(
-        public id: number,
-        public firstName: string,
-        public lastName: string,
-        public age: number,
-        public occupation: string
-    ) {}
+import { Column, Model, Table } from 'sequelize-typescript';
+
+export class Student extends Model<Student>{
+    @Column({ primaryKey: true, autoIncrement: true })
+    id: number;
+  
+    @Column
+    firstName: string;
+  
+    @Column
+    lastName: string;
+
+    @Column
+    age: number;
+
+    @Column
+    occupation: string;
+
+    constructor(id: number, firstName: string, lastName: string, age: number, occupation: string) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.occupation = occupation;
+      }
 }
