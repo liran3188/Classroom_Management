43a4134e170de70722ca03ac11037d601a85f5b1
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-18 15:36:39 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClassesModule } from './classes/classes.module';
 import { StudentsModule } from './students/students.module';
-import { db } from './db';
-import { StudentClass } from './StudentClass.model';
+import { StudentClassModule } from './StudentClass/StudentClass.module'
+
 
 @Module({
-  imports: [ClassesModule, StudentsModule],
+  imports: [ClassesModule, StudentsModule, StudentClassModule],
   controllers: [AppController],
   providers: [AppService],
 })
