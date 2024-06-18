ed6331a703b9e4a8cb491c22a08705f97ab4889e
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-18 15:32:41 +0300

    Update students.repository.ts

--- src/students/students.repository.ts
+++ src/students/students.repository.ts
@@ -1,45 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { Student } from './student.model';
-import { StudentClass } from '../StudentClass.model';
-import { db, sequelize } from '../db'
+import { students } from './student.model';
 
 @Injectable()
 export class StudentRepository {
-  constructor(
-    @InjectModel(Student)
-    private studentModel: typeof Student,
-    @InjectModel(StudentClass)
-    private studentClassModel: typeof StudentClass,
-  ) {}
+  studentModel: any;
+    constructor(
+        @Inject("STUDENT") studentModel: students
+    ) {}
 
-  async findAll(): Promise<Student[]> {
+  async findAll(): Promise<students[]> {
     return this.studentModel.findAll();
   }
 
-  async findById(id: number): Promise<Student> {
+  async findById(id: number): Promise<students> {
     return this.studentModel.findByPk(id);
   }
 
-  async create(studentData: Student): Promise<Student> {
+  async create(studentData: students): Promise<students> {
     return this.studentModel.create(studentData);
   }
 
   async delete(id: number): Promise<number> {
     const deletedRows = await this.studentModel.destroy({ where: { id } });
-    return deletedRows;
-  }
-
-  async getClasses(id: number): Promise<Number[]> {
-    const query = `SELECT class_id FROM public.student_classes WHERE student_id = ${id}`;
-    return db.query(query, { type: sequelize.QueryTypes.SELECT })
-    .then(results => {
-      return (results);
-    })
-  }
-
-  async deleteConnection(id: number): Promise<number> {
-    const deletedRows = await this.studentClassModel.destroy({ where: { studentId: id } });
     return deletedRows;
   }
 }
