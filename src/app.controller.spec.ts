4a013c9fe803bf1bc166c48f5b1641438f69a23f
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-18 15:31:25 +0300

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,58 +1,22 @@
-import {
-    Controller,
-    Post,
-    Body,
-    Get,
-    Param,
-    Patch,
-    Delete,
-  } from '@nestjs/common';
-  
-  import { StudentsService } from './students.service';
-  
-  @Controller('Students')
-  export class StudentsController {
-    constructor(private readonly studentsService: StudentsService) {}
-  
-    @Post()
-    addStudent(
-      @Body('firstName') firstName: string,
-      @Body('lastName') lastName: string,
-      @Body('age') age: number,
-      @Body('occupation') occupation: string,
-    ) {
-      const studentId = Math.floor(Math.random() *1000)
-      this.studentsService.insertStudent(
-        studentId,
-        firstName,
-        lastName,
-        age,
-        occupation
-      );
-    }
-  
-    @Get()
-    getAllStudents() {
-      return this.studentsService.getStudents();
-    }
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
-    @Get(':id')
-    getClass(
-      @Param('id') id: number
-    ) {
-      this.studentsService.getStudent(id)
-    }
+describe('AppController', () => {
+  let appController: AppController;
 
-    @Get()
-    getClasses(
-        @Body('id') studentId: number
-    ) {
-        this.studentsService.getClasses(studentId);
-    }
-  
-    @Delete()
-    deleteStudent(@Body('id') studentId: number) {
-        this.studentsService.deleteStudent(studentId);
-        return null;
-    }
-  }
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
