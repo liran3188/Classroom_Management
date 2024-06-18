f14974c1a4f11b404d9cf54f855478f77250586e
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-18 15:31:55 +0300

    Update student.model.ts

--- src/students/student.model.ts
+++ src/students/student.model.ts
@@ -1,7 +1,7 @@
 import { Column, Model, Table } from 'sequelize-typescript';
 
 @Table
-export class Student extends Model<Student>{
+export class students extends Model<students>{
     @Column({ primaryKey: true, autoIncrement: true })
     id: number;
   
