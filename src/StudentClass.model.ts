08e6deda5bd6a3f28725f87e96c7b4e20372d604
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-18 15:31:11 +0300

    Update StudentClass.model.ts

--- src/StudentClass.model.ts
+++ src/StudentClass.model.ts
@@ -1,29 +1,18 @@
 import { Column, Model, Table } from 'sequelize-typescript';
 
 @Table
-export class students extends Model<students>{
-    @Column({ primaryKey: true, autoIncrement: true })
-    id: number;
+export class studentclass extends Model<studentclass>{
+    @Column({ primaryKey: true })
+    studentId: number;
+
+    @Column({ primaryKey: true })
+    classId: number;
   
-    @Column
-    firstName: string;
-  
-    @Column
-    lastName: string;
-
-    @Column
-    age: number;
-
-    @Column
-    occupation: string;
-
-    constructor(id?: number, firstName?: string, lastName?: string, age?: number, occupation?: string) {
+    constructor(studentId?: number, classId?: number) {
         super();
-        if (id !== undefined && firstName !== undefined && lastName !== undefined && age !== undefined && occupation !== undefined)
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.occupation = occupation;
-      }
+        if (studentId !== undefined && classId !== undefined) {
+          this.studentId = studentId;
+          this.classId = classId;
+        }
+    }
 }
