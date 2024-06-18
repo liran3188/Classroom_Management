9e9a1627ae462d6830db2e215e759cc3a01dac24
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-06-18 15:30:39 +0300

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,58 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+    Controller,
+    Post,
+    Body,
+    Get,
+    Param,
+    Patch,
+    Delete,
+  } from '@nestjs/common';
+  
+  import { StudentsService } from './students.service';
+  
+  @Controller('Students')
+  export class StudentsController {
+    constructor(private readonly studentsService: StudentsService) {}
+  
+    @Post()
+    addStudent(
+      @Body('firstName') firstName: string,
+      @Body('lastName') lastName: string,
+      @Body('age') age: number,
+      @Body('occupation') occupation: string,
+    ) {
+      const studentId = Math.floor(Math.random() *1000)
+      this.studentsService.insertStudent(
+        studentId,
+        firstName,
+        lastName,
+        age,
+        occupation
+      );
+    }
+  
+    @Get()
+    getAllStudents() {
+      return this.studentsService.getStudents();
+    }
 
-describe('AppController', () => {
-  let appController: AppController;
+    @Get(':id')
+    getClass(
+      @Param('id') id: number
+    ) {
+      this.studentsService.getStudent(id)
+    }
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
-
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
-  });
-});
+    @Get()
+    getClasses(
+        @Body('id') studentId: number
+    ) {
+        this.studentsService.getClasses(studentId);
+    }
+  
+    @Delete()
+    deleteStudent(@Body('id') studentId: number) {
+        this.studentsService.deleteStudent(studentId);
+        return null;
+    }
+  }
