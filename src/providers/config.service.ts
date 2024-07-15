841ffb42e3dba5f5ab311bab8479169500dc4173
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-07-15 14:27:07 +0300

    Delete src directory

--- src/providers/config.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { config } from './config.dev';
-
-@Injectable()
-export default class ConfigService {
-    get sequelizeOrmConfig() {
-        return config.database;
-    }
-}
