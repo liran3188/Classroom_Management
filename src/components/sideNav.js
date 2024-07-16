32103a05491a91c4e5c8ce52e969228a7a755b2b
liran3188 <127759430+liran3188@users.noreply.github.com>
2024-07-16 11:47:34 +0300

    Add files via upload

---
+++ src/components/sideNav.js
@@ -0,0 +1,36 @@
+import '../App.css';
+import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+
+
+const SideNav = ({ isOpen, closeSideNav }) => {
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            // Close the side nav if click is outside of it
+            if (isOpen && !event.target.closest('.sidenav')) {
+                closeSideNav();
+            }
+        };
+
+        // Add event listener to listen for clicks on the document
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        // Clean up the event listener on component unmount
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, [isOpen, closeSideNav]);
+
+    return (
+        <>
+            <div className={`overlay ${isOpen ? 'open' : ''}`} onClick={closeSideNav}></div>
+            <div className={`sidenav ${isOpen ? 'open' : ''}`}>
+                <Link to="/classes" onClick={closeSideNav}><h2 className="linkText">Classes</h2></Link>
+                <Link to="/students" onClick={closeSideNav}><h2 className="linkText">Students</h2></Link>
+                <Link to="/create" onClick={closeSideNav}><h2 className="linkText">Create</h2></Link>
+            </div>
+        </>
+    );
+}
+
+export default SideNav;
